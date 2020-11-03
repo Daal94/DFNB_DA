a4936536f7156c23efca4ecde6d2406931f709ac
Daniel <daal94@ldsbc.edu>
2020-11-02 18:06:11 -0700

    “Added enhancements for LDSBC IT 240 - Project 1.4: DFNB - Analyze History - Create v1 of Script Repository”

---
+++ other_scripts/SQLServerCodeHeader.sql
@@ -0,0 +1,23 @@
+/*****************************************************************************************************************
+NAME:    
+PURPOSE: 
+
+SUPPORT: 
+
+MODIFICATION LOG:
+Ver       Date         Author       Description
+-------   ----------   ----------   -----------------------------------------------------------------------------
+
+
+
+RUNTIME: 
+
+NOTES: 
+
+LICENSE: 
+This code is covered by the GNU General Public License which guarantees end users
+the freedom to run, study, share, and modify the code. This license grants the recipients
+of the code the rights of the Free Software Definition. All derivative work can only be
+distributed under the same license terms.
+
+******************************************************************************************************************/
